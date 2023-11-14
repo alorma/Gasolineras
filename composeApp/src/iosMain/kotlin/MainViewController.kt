0afd2f8b7f22d743014c696d6e1c3351db84b3e0
Bernat Borrás <bernat.borras@adevinta.com>
2023-11-14 19:08:01 +0100

    Make iOS app compile

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,4 +1,11 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import com.alorma.gasstations.cache.DatabaseDriverFactory
+import com.alorma.gasstations.domain.GasStationsSdk
 import com.alorma.gasstations.ui.App
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+  val databaseDriverFactory = DatabaseDriverFactory()
+  val sdk = GasStationsSdk(databaseDriverFactory)
+
+  App(sdk)
+}
