53bbaf1968919f5aa628bf1ad68b0862e0ac0b57
Bernat Borrás <bernat.borras@adevinta.com>
2023-11-14 18:11:57 +0100

    Sqldelight plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -4,6 +4,8 @@ plugins {
   alias(libs.plugins.kotlinMultiplatform)
   alias(libs.plugins.androidApplication)
   alias(libs.plugins.jetbrainsCompose)
+  kotlin("plugin.serialization").version("1.9.20")
+  alias(libs.plugins.sqldelight)
 }
 
 kotlin {
