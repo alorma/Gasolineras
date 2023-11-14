ccb842e98829fdf1ba47684e2d1782260ee0b021
Bernat Borrás <bernat.borras@adevinta.com>
2023-11-14 18:41:54 +0100

    Build ktor

--- composeApp/src/commonMain/kotlin/com.alorma.gasstations.network/GasStationsApi.kt
+++ composeApp/src/commonMain/kotlin/com.alorma.gasstations.network/GasStationsApi.kt
@@ -1,2 +1,26 @@
 package com.alorma.gasstations.network
 
+import com.alorma.gasstations.domain.GasStation
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class GasStationsApi {
+  private val httpClient = HttpClient {
+    install(ContentNegotiation) {
+      json(Json {
+        ignoreUnknownKeys = true
+        useAlternativeNames = false
+      })
+    }
+  }
+
+  suspend fun getAllGasStations(): List<GasStation> {
+    return httpClient.get(
+      "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/1141"
+    ).body()
+  }
+}
